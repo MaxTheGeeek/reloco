6d1f73d9f9400954434b05bfa1a84de25f4a35d3
Max Behzadi <69126271+MaxTheGeeek@users.noreply.github.com>
2026-07-30 22:21:25 +0200

    Add gitignore file

--- src/Reloco.Api/Program.cs
+++ src/Reloco.Api/Program.cs
@@ -11,6 +11,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "Reloco.Api v1");
+    });
 }
 
 app.UseHttpsRedirection();
